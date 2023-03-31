345a01eb9bf38fa40b49cbd68e8637257262d09d
efsane <126017552+mustafaeva13@users.noreply.github.com>
2023-03-31 10:31:52 +0400

    Add project files.

---
+++ homework/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.ComponentModel.Design;
+
+namespace array_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task7
+
+
+            //int number;
+            //Console.WriteLine("Enter your number:");
+            //number = int.Parse(Console.ReadLine());
+            //string n = (number % 2 == 0);
+            //Console.WriteLine(number + n);
+
+            //int n=2;
+            //Console.Write(" your number:");
+            //bool Even;
+            //Even = (  n % 2 == 0) ? true:false ;
+            //Console.WriteLine(Even);
+
+            ///TASK6
+
+
+            //string text = "apple";
+            //Console.WriteLine(text );
+            //while (text.Length > 0)
+            //{
+            //    Console.Write(text[0] + "=");
+            //    int word = 0;
+            //    for (int j = 0; j < text.Length; j++)
+            //    {
+            //        if (text[0] == text[j])
+            //        {
+            //            word++;
+            //        }
+            //    }
+            //    Console.WriteLine(word);
+            //    text = text.Replace(text[0].ToString(), string.Empty);
+            //}
+            //Console.ReadLine();
+
+
+
+
+            //TASK 8
+
+
+            //    int wordCount = 0; 
+            //    string str = "Azerbaijan Republic Country";
+            //    str = str.ToLower();
+
+            //    for (int i = 0; i < str.Length; i++)
+            //    {
+            //        if ( str[i] == 'e' )
+            //        {
+            //            wordCount++;
+            //        }
+
+
+            //        else if (str[i] >= 'a' && str[i] <= 'z')
+            //        {
+
+
+            //        }
+            //    }
+            //    Console.WriteLine(" sozdeki e herfi sayi : " + wordCount);
+
+            //}
+
+
+            
+
+
+
+
+        }
+    }
+}
